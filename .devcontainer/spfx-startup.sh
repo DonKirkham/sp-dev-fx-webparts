5e63b11c1e9d788cb3583fca2055abac060d1d47
Don Kirkham <donkirkham@live.com>
2022-11-18 03:09:56 +0000

    Updated devcontainer files

---
+++ .devcontainer/spfx-startup.sh
@@ -0,0 +1,22 @@
+echo
+# echo -e "\e[1;94mInstalling nvm\e[0m"
+# curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.2/install.sh | bash
+# export NVM_DIR="/usr/local/share/nvm"
+# [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+# [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+echo -e "\e[1;94mInstalling Node 16\e[0m"
+nvm i 16
+# npm i -g gulp-cli @pnp/cli-microsoft365
+# echo
+# echo -e "\e[1;94mInstalling Node 14\e[0m"
+# nvm i 14
+# npm i -g gulp-cli @pnp/cli-microsoft365
+# echo
+# echo -e "\e[1;94mInstalling Node 12\e[0m"
+# nvm i 12
+# npm i -g gulp-cli @pnp/cli-microsoft365
+# echo
+# echo -e "\e[1;94mInstalling Node 10\e[0m"
+# nvm i 10
+# npm i -g gulp-cli @pnp/cli-microsoft365
+ZSH_THEME="xiong-chiamiov-plus"
